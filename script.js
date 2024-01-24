2cd72e5f8613279d19f6d06833acf3406d1739d7
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2024-01-24 16:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 //complete this code
+
 class Rectangle {
-	constructor(width,height){
-		this.width=width;
-		this.height=height;
-	}
-	get getWidth(){
-		return this.width;
-	}
-	get getHeight(){
-		return this.height;
-	}
-	getArea(){
-		return this.width*this.width;
-	}
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
 }
+
 
 class Square extends Rectangle {
 	constructor(width,height){
